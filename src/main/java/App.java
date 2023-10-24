dea51bdb4630e66ebdc4d24200416d64813074df
Auceane <haude.auceane15@gmail.com>
2023-10-24 12:18:15 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,25 @@ public class App {
 
     public static void main(String[] args) {
         // A completer
+    	//Test TextBuffer
+    	TextBuffer buffer= new TextBuffer("test");
+    	System.out.println("Test Buffer :\n\t Texte : '"+buffer.toString()+"' ");
+    	System.out.println("\t\t Longueur : "+ buffer.maxP());
+    	
+    	buffer.ins("A",3);
+    	System.out.println("\tApres l'ajout de 'A' à la position 3 : '"+buffer.toString()+"'");
+    	System.out.println("\t\t Longueur : "+ buffer.maxP());
+    	
+    	buffer.del(1, 3);
+    	System.out.println("\tApres la suppression de la position 1 à 3 : '"+buffer.toString()+"'");
+    	System.out.println("\t\t Longueur : "+ buffer.maxP());
+    	
+    	
+    	
+    	
+    	
+    	TextEditor text=new TextEditor("Texte a tester : TesT");//21
+    	
+    	
     }
 }
