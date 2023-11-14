ea152c2403138215a4aa967e51d91e72a8d2044b
Auceane <haude.auceane15@gmail.com>
2023-11-14 11:27:55 +0100

    tests final

--- src/test/java/TextBufferTest.java
+++ src/test/java/TextBufferTest.java
@@ -27,7 +27,66 @@ public class TextBufferTest {
     }
   
   @Test
-  public void should_get_correct_int_when_instanciate_textbuffer_with_text_() {
+  public void should_get_correct_text_when_instanciate_textbuffer_with_text_with_substr_() {
+
+    // Arrange
+
+    TextBuffer buffer = new TextBuffer("Texte de test");
+    
+
+
+    // Act
+    String expectedName = "Texte";
+    String text = buffer.substr(0,5);
+
+
+    // Assert
+    Assertions.assertEquals(expectedName, text);
+
+    }
+  
+  @Test
+  public void should_get_correct_text_when_instanciate_textbuffer_with_text_with_modification_from_ins_() {
+
+    // Arrange
+
+    TextBuffer buffer = new TextBuffer("Texte de test");
+    
+
+
+    // Act
+    buffer.ins("A ", 6);
+    String expectedName = "Texte A de test";
+    String text = buffer.toString();
+
+
+    // Assert
+    Assertions.assertEquals(expectedName, text);
+
+    }
+  
+  @Test
+  public void should_get_correct_text_when_instanciate_textbuffer_with_text_with_modification_from_del_() {
+
+    // Arrange
+
+    TextBuffer buffer = new TextBuffer("Texte de test");
+    
+
+
+    // Act
+    buffer.del(5, 8);
+    String expectedName = "Texte test";
+    String text = buffer.toString();
+
+
+    // Assert
+    Assertions.assertEquals(expectedName, text);
+
+    }
+  
+  @Test
+  public void should_get_correct_length_when_instanciate_textbuffer_with_text_() {
 
     // Arrange
 
@@ -45,22 +104,5 @@ public class TextBufferTest {
 
     }
   
-  @Test
-  public void should_get_correct_text_with_substr_when_instanciate_textbuffer_with_text_() {
-
-    // Arrange
-
-    TextBuffer buffer = new TextBuffer("Texte de test");
-    
-
-
-    // Act
-    String expectedName = "Texte";
-    String text = buffer.substr(0,5);
-
-
-    // Assert
-    Assertions.assertEquals(expectedName, text);
-
-    }
+  
 }
